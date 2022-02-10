f1c5ddb3872ff67a769fa7b0412e5a981376a687
SuperQuentin <quentin.aellen@cpnv.ch>
2022-02-10 16:22:10 +0100

    Add postgres database provider

---
+++ Data/Run4Cause.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using run4cause.Models;
+
+namespace run4cause.Data
+{
+    public class Run4causeContext : DbContext
+    {
+        public Run4causeContext(DbContextOptions<Run4causeContext> options) : base(options) { }
+    }
+}
