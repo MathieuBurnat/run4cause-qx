b69b86615aad1b7acb0c884268a0fcc704e52c8c
SuperQuentin <quentin.aellen@cpnv.ch>
2022-02-10 16:28:28 +0100

    Add User class model with [Annotations]

--- Data/Run4Cause.cs
+++ Data/Run4Cause.cs
@@ -6,5 +6,7 @@ namespace run4cause.Data
     public class Run4causeContext : DbContext
     {
         public Run4causeContext(DbContextOptions<Run4causeContext> options) : base(options) { }
+
+        public virtual DbSet<User> Users { get; set; }
     }
 }
