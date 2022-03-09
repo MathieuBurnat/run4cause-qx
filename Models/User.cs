81303d9af56aa60edb3dbea13abcfcd8e15af646
CARREL Xavier <Xavier.CARREL@cpnv.ch>
2022-03-09 14:20:58 +0100

    Add Identity scaffolding
    Using CLI. Required some tweaking, not sure it's sound

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace run4cause.Models
 {
-    public class User
+    public class User : IdentityUser
     {
         public int Id { get; set; }
 
