d4d2752b1201f1c15e3543a7f63ae686c09a666b
SuperQuentin <quentin.aellen@cpnv.ch>
2022-03-10 09:05:22 +0100

    fix: put roles creation before users to let migration generate

--- Data/Run4Cause.cs
+++ Data/Run4Cause.cs
@@ -7,13 +7,17 @@ namespace run4cause.Data
     {
         public Run4causeContext(DbContextOptions<Run4causeContext> options) : base(options) { }
 
+        public virtual DbSet<Role> Roles { get; set; }
         public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserLogin> UserLogins { get; set; }
+        public virtual DbSet<UserRole> UserRoles { get; set; }
+        public virtual DbSet<UserClaim> UserClaims { get; set; }
         public virtual DbSet<Run> Runs { get; set; }
         public virtual DbSet<Edition> Editions { get; set; }
         public virtual DbSet<Waypoint> Waypoints { get; set; }
         public virtual DbSet<Sponsorship> Sponsorships { get; set; }
         public virtual DbSet<Tracking> Trackings { get; set; }
         public virtual DbSet<RunWaypoint> RunWaypoints { get; set; }
-        public virtual DbSet<Entry> Entries { get; set; }
+
     }
 }
