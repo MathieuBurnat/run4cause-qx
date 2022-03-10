c4529a7673728dc228572fb9b18d532c085d817c
SuperQuentin <quentin.aellen@cpnv.ch>
2022-03-10 10:32:11 +0100

    feat: add initial database migrations

---
+++ Migrations/20220310093018_InitialDb.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace run4cause.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "editions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: true),
+                    latitude = table.Column<double>(type: "double precision", nullable: true),
+                    longitude = table.Column<double>(type: "double precision", nullable: true),
+                    headline = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_editions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    normalized_name = table.Column<string>(type: "text", nullable: true),
+                    concurrency_stamp = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "runs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_runs", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_claims",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    claim_type = table.Column<string>(type: "text", nullable: true),
+                    claim_value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_claims", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_logins",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    login_provider = table.Column<string>(type: "text", nullable: true),
+                    provider_key = table.Column<string>(type: "text", nullable: true),
+                    provider_display_name = table.Column<string>(type: "text", nullable: true),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_logins", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    role_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    phone_number = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: true),
+                    total_distance_covered = table.Column<float>(type: "real", nullable: true),
+                    user_name = table.Column<string>(type: "text", nullable: true),
+                    normalized_user_name = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    normalized_email = table.Column<string>(type: "text", nullable: true),
+                    email_confirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: true),
+                    security_stamp = table.Column<string>(type: "text", nullable: true),
+                    concurrency_stamp = table.Column<string>(type: "text", nullable: true),
+                    phone_number_confirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    two_factor_enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    lockout_end = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    lockout_enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    access_failed_count = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "waypoints",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    latitude = table.Column<double>(type: "double precision", nullable: true),
+                    longitude = table.Column<double>(type: "double precision", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_waypoints", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "edition_run",
+                columns: table => new
+                {
+                    editions_id = table.Column<int>(type: "integer", nullable: false),
+                    runs_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_edition_run", x => new { x.editions_id, x.runs_id });
+                    table.ForeignKey(
+                        name: "fk_edition_run_editions_editions_id",
+                        column: x => x.editions_id,
+                        principalTable: "editions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_edition_run_runs_runs_id",
+                        column: x => x.runs_id,
+                        principalTable: "runs",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "entries",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    register = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    run_id = table.Column<int>(type: "integer", nullable: false),
+                    edition_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_entries", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_entries_editions_edition_id",
+                        column: x => x.edition_id,
+                        principalTable: "editions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_entries_runs_run_id",
+                        column: x => x.run_id,
+                        principalTable: "runs",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_entries_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sponsorships",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    amount_per_kilometer = table.Column<float>(type: "real", nullable: false),
+                    sponsor_id = table.Column<int>(type: "integer", nullable: true),
+                    performer_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_sponsorships", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_sponsorships_users_performer_id",
+                        column: x => x.performer_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "fk_sponsorships_users_sponsor_id",
+                        column: x => x.sponsor_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "run_waypoints",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    instant = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    run_id = table.Column<int>(type: "integer", nullable: false),
+                    waypoint_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_run_waypoints", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_run_waypoints_runs_run_id",
+                        column: x => x.run_id,
+                        principalTable: "runs",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_run_waypoints_waypoints_waypoint_id",
+                        column: x => x.waypoint_id,
+                        principalTable: "waypoints",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "trackings",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    instant = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    waypoint_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_trackings", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_trackings_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_trackings_waypoints_waypoint_id",
+                        column: x => x.waypoint_id,
+                        principalTable: "waypoints",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "id", "concurrency_stamp", "name", "normalized_name" },
+                values: new object[,]
+                {
+                    { 1, "69d1a191-f1e2-4613-9561-474749d92826", "Admin", null },
+                    { 2, "bec7fff4-f946-40cd-bca5-a686f2087403", "Moderator", null },
+                    { 3, "e45977dd-9aac-4edf-8beb-235b7720e13f", "Runner", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "user_roles",
+                columns: new[] { "id", "role_id", "user_id" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 2, 2 },
+                    { 3, 3, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "id", "access_failed_count", "concurrency_stamp", "email", "email_confirmed", "first_name", "last_name", "lockout_enabled", "lockout_end", "normalized_email", "normalized_user_name", "password_hash", "phone_number", "phone_number_confirmed", "security_stamp", "total_distance_covered", "two_factor_enabled", "user_name" },
+                values: new object[,]
+                {
+                    { 1, 0, "de03ec80-0f28-4eb2-b8e9-74028792c046", null, false, "Run4", "admin", false, null, null, null, null, null, false, null, null, false, "default_admin" },
+                    { 2, 0, "df2cf391-73d9-4ac7-b6da-0d72cb600c8a", null, false, "Run4", "moderator", false, null, null, null, null, null, false, null, null, false, "default_moderator" },
+                    { 3, 0, "8168d301-9f98-45a3-b079-f6dea76147c8", null, false, "Run4", "runner", false, null, null, null, null, null, false, null, null, false, "default_Runner" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_edition_run_runs_id",
+                table: "edition_run",
+                column: "runs_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_entries_edition_id",
+                table: "entries",
+                column: "edition_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_entries_run_id",
+                table: "entries",
+                column: "run_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_entries_user_id",
+                table: "entries",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_run_waypoints_run_id",
+                table: "run_waypoints",
+                column: "run_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_run_waypoints_waypoint_id",
+                table: "run_waypoints",
+                column: "waypoint_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_sponsorships_performer_id",
+                table: "sponsorships",
+                column: "performer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_sponsorships_sponsor_id",
+                table: "sponsorships",
+                column: "sponsor_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_trackings_user_id",
+                table: "trackings",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_trackings_waypoint_id",
+                table: "trackings",
+                column: "waypoint_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "edition_run");
+
+            migrationBuilder.DropTable(
+                name: "entries");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropTable(
+                name: "run_waypoints");
+
+            migrationBuilder.DropTable(
+                name: "sponsorships");
+
+            migrationBuilder.DropTable(
+                name: "trackings");
+
+            migrationBuilder.DropTable(
+                name: "user_claims");
+
+            migrationBuilder.DropTable(
+                name: "user_logins");
+
+            migrationBuilder.DropTable(
+                name: "user_roles");
+
+            migrationBuilder.DropTable(
+                name: "editions");
+
+            migrationBuilder.DropTable(
+                name: "runs");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "waypoints");
+        }
+    }
+}
